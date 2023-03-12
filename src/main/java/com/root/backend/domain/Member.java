8b39be8716ae6f235643c8f34fc4a9ee1909a7c9
jungsunghun <jsh951227@gmail.com>
2023-03-13 02:17:20 +0900

    feat: #2 member 엔티티 생성

---
+++ src/main/java/com/root/backend/domain/Member.java
@@ -0,0 +1,34 @@
+package com.root.backend.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public Member(final String username, final String password, final String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
